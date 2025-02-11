452e7022fece0b60e735b848cffd9a20c27db63e
Damian De Lio <damiandelio@emdx.io>
2025-02-11 18:07:50 -0300

    removes tailwindcss and postcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppRouter } from './AppRouter';
-import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <QueryClientProvider client={queryClient}>
       <AppRouter />
     </QueryClientProvider>
